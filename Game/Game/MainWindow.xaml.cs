cd13af5569f50679ada47dd8d088e473e4f3e357
icYFTL <savap0@yandex.ru>
2018-02-20 01:15:21 +0300

    SHP the GAME

--- Game/Game/MainWindow.xaml.cs
+++ Game/Game/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Media;
 
 /* */
 
@@ -22,24 +23,57 @@ namespace Game
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
 
-    
+    static public class Data { static public bool start { get; set; } }
     public partial class MainWindow : Window
     {
-       /* public void generate ()
+        /* public void generate ()
+         {
+             Image enemy = new Image();
+             enemy.Source = new BitmapImage(new Uri(@"C:\Users\icYFTL\source\repos\Game\Game\images\enemy.jpg"));
+             //Random rx = new Random();
+             // Random ry = new Random();
+             // int randy = ry.Next(0,290);
+             Canvas.SetTop(enemy, 0);
+             Canvas.SetLeft(enemy, 0);
+         }*/
+        bool ismuted=false;
+        public MainWindow()
         {
-            Image enemy = new Image();
-            enemy.Source = new BitmapImage(new Uri(@"C:\Users\icYFTL\source\repos\Game\Game\images\enemy.jpg"));
-            //Random rx = new Random();
-            // Random ry = new Random();
-            // int randy = ry.Next(0,290);
-            Canvas.SetTop(enemy, 0);
-            Canvas.SetLeft(enemy, 0);
-        }*/
+            InitializeComponent();
+            if (Data.start == false)
+            {
+                StartWindow w1 = new StartWindow();
+                this.Hide();
+                w1.Show();
+            }
+            
+        }
+        public void audio(bool a)
+        {
+            SoundPlayer sp;
+            string source = "Audio/Das Mörtal - Hotline Miami II.wav"; 
+            int b = randomn(0, 2);
+            if (b == 0)
+            {
+                source = "Audio/Das Mörtal - Hotline Miami II.wav";
+            }
+            if (b == 1)
+            {
+                source = "Audio/Hotline Miami 2 - CS-GO.wav";
+            }
+            
+            sp = new SoundPlayer(@source);
+            if (a == true)
+            {
+                sp.Play();
+            }
+            else sp.Stop();
+        }
         private DispatcherTimer timer = null;
         private int x;
         int speed = 20;
-        int scoreCount=0;
-        private void timerStart()
+        int scoreCount = 0;
+        public void timerStart()
         {
             timer = new DispatcherTimer();  // если надо, то в скобках указываем приоритет, например DispatcherPriority.Render
             timer.Tick += new EventHandler(timerTick);
@@ -48,32 +82,35 @@ namespace Game
         }
 
         private void timerTick(object sender, EventArgs e) //Тут код выполняется циклично с интервалом speed (ms)
-        { 
-                score.Content = scoreCount.ToString();
-                double x = Canvas.GetLeft(enemy);//Вычисление x от enemy
-                double y = Canvas.GetTop(enemy);//Вычесление y от enemy
-                x -= 5; //Единичный шаг enemy
-                Canvas.SetLeft(enemy, x); //Установка новой координаты  enemy
-                if (x < -200) //Если enemy вышел за пределы canvas
-                {
-                 
-                        Canvas.SetTop(enemy, randomn(0, 200));//Перемещаем enemy на рандомную позицию по Y (от 0 до 200)
-                        Canvas.SetLeft(enemy, 600);//Перемещаем enemy на по X (600)
-                        speed = randomn(1, 10); //Генерим скорость движения enemy (1 ms - 10 ms)
-                        timer.Interval = new TimeSpan(0, 0, 0, 0, speed);//Установка скорости
-                        enemy.Height = randomn(50, 200); enemy.Width = randomn(50, 200); //Рандомная генерация размера enemy
-                        scoreCount++;
-                   
-                    
-                }
-            
+        {
+            score.Content = scoreCount.ToString();
+            double x = Canvas.GetLeft(enemy);//Вычисление x от enemy
+            double y = Canvas.GetTop(enemy);//Вычесление y от enemy
+            x -= 5; //Единичный шаг enemy
+            Canvas.SetLeft(enemy, x); //Установка новой координаты  enemy
+            if (x < -200) //Если enemy вышел за пределы canvas
+            {
+
+                Canvas.SetTop(enemy, randomn(0, 200));//Перемещаем enemy на рандомную позицию по Y (от 0 до 200)
+                Canvas.SetLeft(enemy, 600);//Перемещаем enemy на по X (600)
+                speed = randomn(1, 10); //Генерим скорость движения enemy (1 ms - 10 ms)
+                timer.Interval = new TimeSpan(0, 0, 0, 0, speed);//Установка скорости
+                enemy.Height = randomn(50, 200); enemy.Width = randomn(50, 200); //Рандомная генерация размера enemy
+                scoreCount++;
+
+
+            }
+
             check();//Проверка на столкновение
         }
-        public MainWindow()
+        public void resolve ()
         {
-            InitializeComponent();
-            timerStart();
-     
+            try
+            {
+               int a =  Convert.ToInt32(System.IO.File.ReadAllText(@"bestscore.ini").ToString());
+               if (a < scoreCount) { System.IO.File.WriteAllText(@"bestscore.ini", scoreCount.ToString()); }
+            }
+            catch (Exception ex) { System.IO.File.WriteAllText(@"bestscore.ini", "0"); return; }
         }
         public void check ()
         {
@@ -83,9 +120,30 @@ namespace Game
             double playery = Canvas.GetTop(player);
             if (enemyx == playerx+50)
             {
-                for (int i = 0; i < 50; i++) { if (enemyy == playery || enemyy - i == playery) { MessageBox.Show("You lose!"); break; } }
+                for (int i = 0; i < 50; i++) { if (enemyy == playery || enemyy - i == playery) {
+                        MessageBox.Show("You lose!");
+                        audio(false);
+                        resolve();
+                        timer.Stop();
+                        this.Hide();
+                        StartWindow s1 = new StartWindow();
+                        s1.scoreUpdater();
+                        s1.Show();
+                        break;
+                    }
+                }
                 for (int i = 0; i < 75; i++)
-                     if (enemyy == playery || enemyy+i == playery) { MessageBox.Show("You lose!"); break; }
+                     if (enemyy == playery || enemyy+i == playery) {
+                        MessageBox.Show("You lose!");
+                        audio(false);
+                        resolve();
+                        timer.Stop();
+                        this.Hide();
+                        StartWindow s1 = new StartWindow();
+                        s1.scoreUpdater();
+                        s1.Show();
+                        break;
+                    }
             }
 
         }
@@ -120,6 +178,19 @@ namespace Game
             Canvas.SetTop(player, y);
         }
 
-
+        private void mute_Click(object sender, RoutedEventArgs e)
+        {
+            if (ismuted == false)
+            {
+                audio(false); ismuted = true;
+                //mute_img.Source = new Uri("Pictures/muted.png", UriKind.Relative);
+            }
+            else
+            {
+                audio(true);
+                ismuted = false;
+               // mute_img.Source = @"Pictures/unmuted.png";
+            }
+        }
     }
 }
---
+++ Game/Game/StartWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Game
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            scoreUpdater();
+        }
+        
+
+        public void scoreUpdater ()
+        {
+            try
+            {
+                bestscore.Content = System.IO.File.ReadAllText(@"bestscore.ini").ToString();
+            }
+            catch (Exception ex) { bestscore.Content = "0"; }
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Hide();
+            Data.start = true;
+            MainWindow mw = new MainWindow();
+            mw.timerStart();
+            mw.audio(true);
+            mw.Show();
+            
+
+        }
+    }
+}
